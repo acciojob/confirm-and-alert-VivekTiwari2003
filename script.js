623de0dbabb3f3679c95970f35f2609429a3a389
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-05-04 18:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+if (confirm("You clicked OK. proceeding...") == true) {
+				userPreference = "Data saved successfully!";
+			} else {
+				userPreference = "You clicked Cancel. Exiting...";
+			}
